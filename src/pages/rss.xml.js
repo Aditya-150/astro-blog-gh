67676b3aa4b0994491e67306a207cc1026aef7c3
Aditya Prasad Mohanty <78409764+Aditya-150@users.noreply.github.com>
2023-09-18 05:39:21 +0530

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/astro-blog-gh/blog/${post.slug}/`,
+			link: `/blog/${post.slug}/`,
 		})),
 	});
 }
